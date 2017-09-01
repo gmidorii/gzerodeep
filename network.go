c06478fa4e58a158bfc192921f86046fbd1fa07f
tagasouichiro <stitow14@gmail.com>
2017-09-01 22:28:38 +0900

    Add sigmoid

--- network.go
+++ network.go
@@ -1,11 +1,13 @@
 package main
 
+import "math"
+
 func network() {
 
 }
 
-func sigmoid(x float64) {
-
+func sigmoid(x float64) float64 {
+	return 1 / (1 + math.Exp(-x))
 }
 
 func softmax(a float64) {
